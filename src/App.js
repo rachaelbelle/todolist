a5f5de77a2c6268679a57acb90b9ee37fc801e8e
YvelStLaurent <rachaelbelle@gmail.com>
2019-11-12 15:57:39 -0500

    building handlers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { directive } from '@babel/types';
+// import { directive } from '@babel/types';
 
-class App extends React.Component{
-  render(){
-    return(
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+  render() {
+    return (
       <div className="App">
         <header>
-        <form id="to-do-form">
-          <input type="text" placeholder="Enter Text"/>
-          <button type="submit">Add</button>
-        </form>
-      </header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter Text"
+            value={this.state.currentItem.text}
+            onChange={this.handleInput}/>
+            <button type="submit">Add</button>
+          </form>
+        </header>
       </div>
-      );
+    );
   }
 }
 
