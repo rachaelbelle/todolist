f671c8944531f5609d79f6752a3231df83f6e8b1
YvelStLaurent <rachaelbelle@gmail.com>
2019-11-12 15:37:51 -0500

    first styles

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { directive } from '@babel/types';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="App">
+        <header>
+        <form id="to-do-form">
+          <input type="text" placeholder="Enter Text"/>
+          <button type="submit">Add</button>
+        </form>
+      </header>
+      </div>
+      );
+  }
+}
+
+export default App;
