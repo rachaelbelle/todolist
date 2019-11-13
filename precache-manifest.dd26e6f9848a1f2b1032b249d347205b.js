0ea3be67780835ce7b650e9193cf4cab6062aa97
YvelStLaurent <rachaelbelle@gmail.com>
2019-11-13 14:13:38 -0500

    Updates

---
+++ precache-manifest.dd26e6f9848a1f2b1032b249d347205b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f71b548b7eed03db2174d838059515c",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "1d14288278e9bc6fea7e",
+    "url": "/todolist/static/css/main.c9d353af.chunk.css"
+  },
+  {
+    "revision": "ce5ecba05599f80c33b2",
+    "url": "/todolist/static/js/2.5231abe1.chunk.js"
+  },
+  {
+    "revision": "1d14288278e9bc6fea7e",
+    "url": "/todolist/static/js/main.c6b15454.chunk.js"
+  },
+  {
+    "revision": "773db8ed56ba2d1300d5",
+    "url": "/todolist/static/js/runtime-main.5baa8871.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/todolist/static/media/logo.25bf045c.svg"
+  }
+]);
