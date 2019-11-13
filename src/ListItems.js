68f343dbce1934071ab5424d8d3ba001d941e3c2
YvelStLaurent <rachaelbelle@gmail.com>
2019-11-13 13:41:32 -0500

    add and delete functionality

--- src/ListItems.js
+++ src/ListItems.js
@@ -5,11 +5,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function ListItems(props) {
   const items = props.items;
   const listItems = items.map(item => {
-    return <div className="list" key="item.key">
+    return <div className="list" key={item.key}>
       <p>{item.text}
-        <span>
-          <FontAwesomeIcon className="faicons" icon='trash' />
-        </span>
+      <span>
+        <FontAwesomeIcon className="faicons"
+         icon='trash'
+         onClick={ () => props.deleteItem(item.key) }
+           />
+      </span>
       </p>
     </div>
   })
